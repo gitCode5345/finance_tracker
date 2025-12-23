448fa2a49356b2d6968593a656d8605413db2987
Dmytro Mariukhna <maryukhna05@gmail.com>
2025-12-23 15:22:47 +0200

    DB settings, added methods for registration, login, and obtaining user information

---
+++ lib/core/database_tables.dart
@@ -0,0 +1,6 @@
+abstract class DatabaseTablesName {
+  static final tableCategories = 'Categories';
+  static final tableUsers = 'Users';
+  static final tableTransactions = 'Transactions';
+  static final tableBalances = 'Balances';
+}
