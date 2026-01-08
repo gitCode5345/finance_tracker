19e884028c71919dac5c0339bed800379ee6f4a3
Dmytro Mariukhna <maryukhna05@gmail.com>
2026-01-08 12:08:09 +0200

    removed sqlite-based database implementation

--- lib/core/database_tables.dart
+++
@@ -1,6 +0,0 @@
-abstract class DatabaseTablesName {
-  static final tableCategories = 'Categories';
-  static final tableUsers = 'Users';
-  static final tableTransactions = 'Transactions';
-  static final tableBalances = 'Balances';
-}
