80a3282b7478716c61a284cbaa5bc0de87ed2890
Dmytro Mariukhna <maryukhna05@gmail.com>
2026-01-28 15:44:47 +0200

    feat: transaction card implemented

---
+++ lib/presentation/widgets/transaction_item_widget.dart
@@ -0,0 +1,55 @@
+import 'package:finance_tracker/data/models/transaction/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:intl/intl.dart';
+
+Widget buildTransactionItem(Transaction tx) {
+  final date = tx.date;
+  final fullMonthName = DateFormat('MMMM dd, yyyy').format(date.toLocal());
+
+  return ListTile(
+    title: IntrinsicHeight(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SvgPicture.asset(
+            tx.category?.icon ?? '',
+            width: 57,
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    tx.type.name,
+                    style: const TextStyle(
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w500,
+                      fontSize: 15.0,
+                      color: Color.fromRGBO(5, 34, 36, 1.0),
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                  ),
+                  Text(
+                    '${DateFormat('HH:mm').format(date.toLocal())} - $fullMonthName',
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                    style: const TextStyle(fontSize: 12),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Text(
+            tx.amount.toString(),
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    ),
+  );
+}
